38b1f7fe24f37f2dd6187b991e1d4017a337986f
asshah4 <mrshahman@gmail.com>
2019-12-21 09:26:02 -0500

    better trimming file made

--- trim-data.sh
+++ trim-data.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-# File name
-FILE='test.txt'
-
-# Extract time stamp
-declare TIME=$(sed '2q;d' $FILE)
-sed '2q;d' $FILE > tmp.txt
-
-# Skip first headings
-tail -n +4 $FILE | awk '{print $1,$2}' >> tmp.txt
-
-# Multiple visits are subfolders
-for f in ./*/*/*ECG.txt; do
-	cd "$f"
-done
-
-
-# To clean up all the parameter files that are old
-for d in ./proc_data/*/; do (cd "$d" && ls -t Param*.csv | sed '1d' | xargs rm); done
-for d in ./proc_data/*/; do (cd "$d" && ls -t Param*.tex | sed '1d' | xargs rm); done
-
-# Removed WIndows file
-for d in ./proc_data/*/; do (cd "$d" && ls -t Removed*.csv | sed '1d' | xargs rm); done
-
-# HRV result files
-for d in ./proc_data/*/; do (cd "$d" && ls -t *HRV_results*.csv | sed '1d' | xargs rm); done
-
-
-
-
-
+# Find all the ecg files
+# Get rid of the useless spaces
+find ./data/* -type f -name "*ECG.txt" -print0 | \
+	xargs -0 sed -i -e "s/ -9999//g"
